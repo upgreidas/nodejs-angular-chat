2d81210a1154c37b9a803456b26ab181cbb54e3d
upgreidas <gungaudas@gmail.com>
2020-08-09 02:51:13 +0300

    Implement client login function

--- apps/client/src/app/components/pages/login-page/login-page.component.ts
+++ apps/client/src/app/components/pages/login-page/login-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { LoginResponse } from '../../../interfaces/responses/login-response';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { BootstrapService } from '../../../services/bootstrap.service';
 
 @Component({
   selector: 'chat-login-page',
@@ -14,12 +18,29 @@ export class LoginPageComponent implements OnInit {
     password: new FormControl('', [Validators.required]),
   });
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private bootstrapService: BootstrapService,
+    private authenticationService: AuthenticationService,
+  ) { }
 
   ngOnInit(): void {
   }
 
   login() {
+    this.http.post<LoginResponse>('/api/auth/login', this.loginForm.value)
+      .subscribe(res => {
+        if(res.token) {
+          this.authenticationService.setToken(res.token);
+
+          this.bootstrapService.load()
+            .subscribe(res => {
+              
+            });
+        }
+      });
+
     this.router.navigate(['/channels', 'main']);
   }
 
